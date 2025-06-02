55442c44100bde9d630b4825760bfd0f67075071
Phrit Nakgate <boss5500216@gmail.com>
2025-06-02 14:25:02 +0700

    ep.2

---
+++ src/main/java/th/co/cdg/SimpleApplication/model/User.java
@@ -0,0 +1,8 @@
+package th.co.cdg.SimpleApplication.model;
+
+public class User {
+    private String name;
+
+    public String getName() { return name;}
+    public void setName(String name) { this.name = name; }
+}
--- src/main/java/th/co/cdg/SimpleApplication/presentation/SimpleController.java
+++ src/main/java/th/co/cdg/SimpleApplication/presentation/SimpleController.java
@@ -1,14 +1,29 @@
 package th.co.cdg.SimpleApplication.presentation;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import th.co.cdg.SimpleApplication.model.User;
 
 @RestController
 public class SimpleController {
-    @GetMapping(value="hello-world")
-    public ResponseEntity<String> getHelloWorld(@RequestParam(name="name", defaultValue = "World") String name) {
-        return ResponseEntity.ok().body("Hello "+name+"!");
+    @GetMapping(value="hello-with-name/{name}")
+    public ResponseEntity<String> getHelloWorld(@PathVariable(name="name") String name) {
+        try{
+            return ResponseEntity.ok().body("Hello "+name+"!");
+        }catch(HttpClientErrorException clientErrorException){
+            if(clientErrorException.getStatusCode() == HttpStatus.BAD_REQUEST){
+                return ResponseEntity.badRequest().body("Please provide a valid name.");
+            }else{
+                return ResponseEntity.notFound().build();
+            }
+        }
+    }
+
+    @PostMapping(value="hello-with-body", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postHelloWorld(@RequestBody User user) {
+        return ResponseEntity.ok().body("Helo "+ user.getName() + "!");
     }
 }
