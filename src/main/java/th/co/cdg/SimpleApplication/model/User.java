ce954ab557f375d48f0436df3e2af206c1ea2170
Phrit Nakgate <boss5500216@gmail.com>
2025-06-02 16:47:10 +0700

    ep.3

--- src/main/java/th/co/cdg/SimpleApplication/model/User.java
+++ src/main/java/th/co/cdg/SimpleApplication/model/User.java
@@ -1,8 +1,17 @@
 package th.co.cdg.SimpleApplication.model;
 
 public class User {
+    private int id;
     private String name;
+    private String tel;
 
+    //Getter Methods
+    public int getId() { return id; }
     public String getName() { return name;}
+    public String getTel() { return tel;}
+
+    //Setter Methods
+    public void setId(int id) { this.id = id; }
     public void setName(String name) { this.name = name; }
+    public void setTel(String tel) { this.tel = tel; }
 }
--- src/main/java/th/co/cdg/SimpleApplication/presentation/SimpleController.java
+++ src/main/java/th/co/cdg/SimpleApplication/presentation/SimpleController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import th.co.cdg.SimpleApplication.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 @RestController
 public class SimpleController {
+
+    // Hello World Endpoints \\
     @GetMapping(value="hello-with-name/{name}")
     public ResponseEntity<String> getHelloWorld(@PathVariable(name="name") String name) {
         try{
@@ -26,4 +32,56 @@ public class SimpleController {
     public ResponseEntity<String> postHelloWorld(@RequestBody User user) {
         return ResponseEntity.ok().body("Helo "+ user.getName() + "!");
     }
+
+    // User Endpoints \\
+    private final List<User> users = new ArrayList<>();
+
+    private boolean isUserExist(int id){
+        return users.stream().anyMatch(user -> user.getId() == id);
+    }
+
+    private User findUserById(int id) {
+        return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
+    }
+
+    @GetMapping(value="/api/users")
+    public ResponseEntity<List<User>> getUsers() {
+        if(users.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.ok().body(users);
+        }
+
+    }
+
+    @PostMapping(value="/api/create-user")
+    public ResponseEntity<String> createUser(@RequestBody User user){
+       //Validation
+        if(user.getId() <= 0 || user.getName() == null || user.getTel() == null){
+            return ResponseEntity.badRequest().body("Invalid user data provided.");
+        }
+        if (isUserExist(user.getId())) {
+            return ResponseEntity.badRequest().body("User with ID " + user.getId() + " already exists.");
+        }
+        users.add(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully with ID: " + user.getId());
+    }
+    @GetMapping(value="/api/user/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        if(isUserExist(id)){
+            return ResponseEntity.ok().body(findUserById(id));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping(value="/api/user/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable int id) {
+        if(isUserExist(id)){
+            User user = findUserById(id);
+            users.remove(user);
+            return ResponseEntity.ok().body("User with ID " + id + " deleted successfully.");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
